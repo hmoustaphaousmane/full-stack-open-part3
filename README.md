80ef7ab816a83466a3546fa1f0658f0cd1d6acae
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-09-10 17:17:21 +0100

    feat: submission for part3: phonebook steps 9, 10 and 11
    - uibuild

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # full-stack-open-part3
+
+* Online link of the app is [here](https://full-stack-open-part3-3wh8.onrender.com/)
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
+app.use(express.static('dist'))
 
 app.use(express.json())
 
@@ -81,6 +84,6 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
